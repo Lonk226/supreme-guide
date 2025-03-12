01c8f953e8681eb275364a52d189104b6b2a51b3
Lincoln Jones <lincolnjones226@gmail.com>
2025-03-12 09:26:51 -0400

    initial commit

---
+++ dashlevels/Scripts/dashgem.gd
@@ -0,0 +1,11 @@
+extends Node2D
+
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+
+signal reset_dash
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	reset_dash.emit()
+	animated_sprite.play("active")
+	await get_tree().create_timer(0.6).timeout
+	animated_sprite.play("idle")
