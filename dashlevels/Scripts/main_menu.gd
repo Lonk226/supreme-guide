3120f9b5e8435dbc08b1aaf2cd96f7bbb3126e30
Lincoln Jones <lincolnjones226@gmail.com>
2025-03-14 10:16:53 -0400

    controller support

--- dashlevels/Scripts/main_menu.gd
+++ dashlevels/Scripts/main_menu.gd
@@ -1,7 +1,8 @@
 extends Control
 
 
-
+func _physics_process(delta: float) -> void:
+	start()
 
 func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/Levels/level_1.tscn")
@@ -11,3 +12,7 @@ func _on_start_pressed() -> void:
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
+
+func start():
+	if Input.is_action_just_pressed("pause"):
+		get_tree().change_scene_to_file("res://Scenes/Levels/level_1.tscn")
