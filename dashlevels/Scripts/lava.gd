120670f4f44c0278b6fe6e43cc87040011a2cc70
Lincoln Jones <lincolnjones226@gmail.com>
2025-05-13 10:15:53 -0400

    credits

--- dashlevels/Scripts/lava.gd
+++ dashlevels/Scripts/lava.gd
@@ -1,6 +1,11 @@
 extends Area2D
 
 var speed = -50
+var finalspeed = -80
 
 func _process(delta: float) -> void:
-	translate(Vector2(0, speed * delta))
+	var current_scene_file = get_tree().current_scene.scene_file_path
+	if current_scene_file == "res://Scenes/Levels/level_15.tscn":
+		translate(Vector2(0, finalspeed * delta))
+	else:
+		translate(Vector2(0, speed * delta))
--- dashlevels/Scripts/level_10.gd
+++ dashlevels/Scripts/level_10.gd
@@ -3,4 +3,4 @@ extends Node2D
 
 func _on_enemy_defeat() -> void:
 	await get_tree().create_timer(1).timeout
-	get_tree().change_scene_to_file("res://Scenes/Levels/level_11.tscn")
+	get_tree().change_scene_to_file("res://Scenes/end.tscn")
